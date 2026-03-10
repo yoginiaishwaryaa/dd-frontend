4f4e00ca42cd9b54c3c36d257bad53278af15aab
Yogini Aishwaryaa P T S <yoginianithakumar@gmail.com>
2026-03-10 18:32:29 +0530

    Update GitHub link for delta-docs-auto-gen

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -264,7 +264,7 @@ export default function Dashboard() {
                                         Manage Repos
                                     </Button>
                                 </Link>
-                                <a href="https://github.com/apps/delta-docs/installations/new">
+                                <a href="https://github.com/apps/delta-docs-auto-gen/installations/new">
                                     <Button className="btn-primary-delta">
                                         <Plus className="size-4 mr-2" />
                                         Link Repository
